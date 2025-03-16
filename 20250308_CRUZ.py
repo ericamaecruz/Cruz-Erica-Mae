377fdd94c454cd19879187ac2da65a90851c454c
ericamaecruz <ericamaegcruz@gmail.com>
2025-03-16 15:01:55 +0800

    Update 20250308_CRUZ.py

--- 20250308_CRUZ.py
+++ 20250308_CRUZ.py
@@ -1,6 +1,6 @@
 import scipy as sts
 import numpy as np
-import matpotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 mu = np.linspace(1.65, 1.8, num = 50)
 test = np.linspace(0, 2)
@@ -11,7 +11,7 @@ beta_dist = sts.beta.pdf(mu, 2, 5, loc = 1.65, scale = 0.2)
 beta_dist = beta_dist/beta_dist.sum()
 plt.plot(mu, beta_dist, label = 'Beta Dist')
 plt.plot(mu, uniform_dist, label = 'Uniform Dist')
-plt.xlabel("Value of $\mu$ in meters")
+plt.xlabel(r"Value of $\mu$ in meters")
 plt.ylabel("Probability Density")
 plt.legend()
 
@@ -23,9 +23,9 @@ def likelihood_func(datum, mu):
 likelihood_out = likelihood_func(1.7, mu)
 
 plt.plot(mu, likelihood_out)
-plt.title("Likelihood of $\mu$ given observation 1.7m")
+plt.title(r"Likelihood of $\mu$ given observation 1.7m")
 plt.ylabel("Probability Density/Likelihood")
-plt.xlabel("Value of $\mu$")
+plt.xlabel(r"Value of $\mu$")
 plt.show()
 
 
@@ -33,6 +33,6 @@ import scipy as sp
 
 unnormalized_posterior = likelihood_out * uniform_dist
 plt.plot(mu, unnormalized_posterior)
-plt.xlabel("$\mu$ in meters")
+plt.xlabel(r"$\mu$ in meters")
 plt.ylabel("Unnormalized Posterior")
 plt.show()
